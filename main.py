b517acadb7653baab17df0822b1e47714154b36e
imane <imane@imanes-MacBook-Air.local>
2023-12-11 15:44:25 +0100

    implemented ratio logic between incorrect and correct answers allowing handling of which cards needs to be reviewed the most

--- main.py
+++ main.py
@@ -6,26 +6,49 @@ class Flashcard:
         self.question = question
         self.choices = choices
         self.correct_answer = correct_answer
-        self.incorrect_attempts = 0  # Keep track of incorrect attemps so that this flashcard shows up more often 
+        self.incorrect_attempts = 0
+        self.correct_attempts = 0
 
     def ask_question(self):
         print(self.question)
         for idx, choice in enumerate(self.choices, 1):
             print(f"{idx}. {choice}")
-        user_answer = int(input("Your answer (number): "))
-        if self.choices[user_answer - 1] == self.correct_answer:
-            print("Correct!")
-        else:
-            print("Incorrect. The correct answer was:", self.correct_answer)
-            self.incorrect_attempts += 1  # Increase the incorrect attempt count
+
+        while True:
+            try:
+                user_answer = int(input("Your answer (number): "))
+                if 1 <= user_answer <= len(self.choices):
+                    if self.choices[user_answer - 1] == self.correct_answer:
+                        print("Correct!")
+                        self.correct_attempts += 1
+                    else:
+                        print("Incorrect. The correct answer was:", self.correct_answer)
+                        self.incorrect_attempts += 1
+                    break
+                else:
+                    print("Please enter a number between 1 and", len(self.choices))
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+
 
     def to_dict(self):
         return {
             "question": self.question,
             "choices": self.choices,
             "correct_answer": self.correct_answer,
-            "incorrect_attempts": self.incorrect_attempts
+            "incorrect_attempts": self.incorrect_attempts,
+            "correct_attempts": self.correct_attempts
         }
+
+
+def select_flashcard(flashcard_bank):
+    if not flashcard_bank:
+        return None
+    # Sort flashcard based on incorrect/correct ratio attempts
+    # Flashcards with a higher ratio (more incorrect than correct) are prioritized
+    flashcard_bank.sort(key=lambda x: (x.incorrect_attempts + 1) / (x.correct_attempts + 1), reverse=True)
+    # Select from the top prioritized flashcards
+    return random.choice(flashcard_bank[:max(3, len(flashcard_bank)// 2)])
 
 
 def add_flashcard():
@@ -72,17 +95,13 @@ def main():
             save_flashcards_to_file(flashcard_bank)
         elif choice == '2':
             if flashcard_bank:
-                # Sort flashcards by incorrect attempts in descending order, so that it can prioritize which flashcard to display 
-                flashcard_bank.sort(key = lambda x: x.incorrect_attempts, reverse=True)
-                # Prioritize the flashcards with the most incorrect attempts
-                random_flashcard = random.choice(flashcard_bank[:max(3, len(flashcard_bank)//2)])
-                random_flashcard.ask_question()
+                random_flashcard = select_flashcard(flashcard_bank)
+                if random_flashcard:
+                    random_flashcard.ask_question()
+                    save_flashcards_to_file(flashcard_bank)
+                else:
+                    print("You have no flashcard available. Please add some first")
 
-                continue_review = input("Do you want to review another flashcard? (yes/no): ").lower()
-                if continue_review != 'yes':
-                    continue  # Go back to the main menu
-            else:
-                print("No flashcards available. Please add some first.")
         elif choice == '3':
             print("Thank you for using Wizzpy. Goodbye!")
             break
